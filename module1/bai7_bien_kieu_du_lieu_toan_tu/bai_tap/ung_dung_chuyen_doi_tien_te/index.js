c8a3d8cd4ef6a54451bc5bb1cf0f744c9c59c141
A0321I1-Lê Công Hậu <leconghau095@gmail.com>
2021-04-07 22:51:43 +0700

    nop bai tap

---
+++ module1/bai7_bien_kieu_du_lieu_toan_tu/bai_tap/ung_dung_chuyen_doi_tien_te/index.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    var amount = document.getElementById('amount').value;
+    var country = document.getElementById("country").value;
+    var currency = document.getElementById("currency").value;
+    var result = amount;
+    if(country=="VietNam" && currency=="USD"){
+        result = amount / 23092;
+        currency = "USD";
+    }
+    if(country=="USA" && currency=="VND"){
+        result = amount * 23092;
+        currency = "VND";
+    }
+    document.getElementById("text").innerHTML = result + " " + currency;
+}
