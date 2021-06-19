40d81eda377a41415da19aad75ea2f0bc63e9f38
A0321I1-Lê Công Hậu <leconghau095@gmail.com>
2021-06-19 08:21:47 +0700

    nop bai tap

---
+++ module2/src/introduction_to_java/bai_tap/HienThiLoiChao.java
@@ -0,0 +1,12 @@
+package introduction_to_java.bai_tap;
+
+import java.util.Scanner;
+
+public class HienThiLoiChao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello: " + name);
+    }
+}
--- module2/src/introduction_to_java/thuc_hanh/KiemTraNamNhuan.java
+++ module2/src/introduction_to_java/thuc_hanh/KiemTraNamNhuan.java
@@ -1,4 +1,24 @@
 package introduction_to_java.thuc_hanh;
 
+import java.util.Scanner;
+
 public class KiemTraNamNhuan {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+        boolean isLeapYear = false;
+        if(year%4==0){
+            if(year%100!=0){
+                isLeapYear = true;
+            }else {
+                isLeapYear = false;
+            }
+        }
+        if(isLeapYear){
+            System.out.println("Year " + year + " is a leap year");
+        } else {
+            System.out.println("Year " + year + " is not a leap year");
+        }
+    }
 }
