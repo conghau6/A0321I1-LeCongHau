61c4ed96293ca8ede719ccfb85274b4dd6df28fb
A0321I1-Lê Công Hậu <leconghau095@gmail.com>
2021-06-19 19:20:27 +0700

    nop bai tap

--- module2/src/_2_vong_lap_trong_java/thuc_hanh/UngDungTinhTienLaiChoVay.java
+++ module2/src/_2_vong_lap_trong_java/thuc_hanh/UngDungTinhTienLaiChoVay.java
@@ -4,6 +4,17 @@ import java.util.Scanner;
 
 public class UngDungTinhTienLaiChoVay {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập số tiền gữi: ");
+        double so_tien_gui = scanner.nextDouble();
+        System.out.print("Nhập tỉ lệ lãi suất (%/năm): ");
+        double ti_le_lai_suat = scanner.nextDouble();
+        System.out.print("Nhập số tháng gũi: ");
+        int so_thang_gui = scanner.nextInt();
+        double so_tien_lai = 0;
+        for(int i=0; i < so_thang_gui; i++) {
+            so_tien_lai += so_tien_gui * (ti_le_lai_suat/100)/12 * so_thang_gui;
+        }
+        System.out.println("Số tiền lãi = " + so_tien_lai);
     }
 }
