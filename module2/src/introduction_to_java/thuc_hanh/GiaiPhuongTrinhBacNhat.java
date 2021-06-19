6a28458a21cce2fe11faf46ae97a1b66ee53cac6
A0321I1-Lê Công Hậu <leconghau095@gmail.com>
2021-06-19 07:07:34 +0700

    nop bai tap

---
+++ module2/src/introduction_to_java/thuc_hanh/GiaiPhuongTrinhBacNhat.java
@@ -0,0 +1,32 @@
+package introduction_to_java.thuc_hanh;
+
+import java.util.Scanner;
+
+public class GiaiPhuongTrinhBacNhat {
+    static double a,b,c, solution;
+    static private void nhap(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("a: ");
+        a = scanner.nextDouble();
+        System.out.print("b: ");
+        b = scanner.nextDouble();
+        System.out.print("c: ");
+        c = scanner.nextDouble();
+    }
+    static private void giai(){
+        if (a != 0) {
+            double answer = (c - b) / a;
+            System.out.printf("Equation pass with x = %f!\n", answer);
+        } else {
+            if (b == 0) {
+                System.out.print("The solution is all x!");
+            } else {
+                System.out.print("No solution!");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        GiaiPhuongTrinhBacNhat.nhap();
+        GiaiPhuongTrinhBacNhat.giai();
+    }
+}
--- module2/src/introduction_to_java/thuc_hanh/SuDungToanTu.java
+++ module2/src/introduction_to_java/thuc_hanh/SuDungToanTu.java
@@ -1,5 +1,17 @@
 package introduction_to_java.thuc_hanh;
 
+import java.util.Scanner;
+
 public class SuDungToanTu {
-    
+    public static void main(String[] args) {
+        float width;
+        float height;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter width: ");
+        width = scanner.nextFloat();
+        System.out.print("Enter height: ");
+        height = scanner.nextFloat();
+        float area = width * height;
+        System.out.println("Area is: " + area);
+    }
 }
