3b331d4831537deb962479a81da4ed1325d183c8
A0321I1-Lê Công Hậu <leconghau095@gmail.com>
2021-06-20 18:57:29 +0700

    nop bai tap

--- module2/src/_3_mang_va_phuong_thuc_trong_java/bai_tap/XoaPhanTuCuaMang.java
+++ module2/src/_3_mang_va_phuong_thuc_trong_java/bai_tap/XoaPhanTuCuaMang.java
@@ -6,20 +6,27 @@ import java.util.Scanner;
 public class XoaPhanTuCuaMang {
     public static int[] delete(int value, int[] array){
         int index = -1;
-        for(int i=0; i<array.length; i++){
-            if(array[i]==value) index = i;
+        for(int i=0; i<array.length; i++) {
+            if (array[i] == value) index = i;
         }
-        if(index!=-1){
-            for(int i=index; i<array.length-1; i++){
-                array[i] = array[i+1];
+        int[] newArray = new int[array.length-1];
+        if(index!=-1) {
+            for (int i = index; i < array.length - 1; i++) {
+                array[i] = array[i + 1];
             }
+            for(int i=0; i<newArray.length; i++) {
+                newArray[i] = array[i];
+            }
+            System.out.println("Deleted " + value);
+            return newArray;
         }
+        System.out.println("Not found " + value);
         return array;
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] arr = {1,2,5,7,8,23};
-        delete(7, arr);
-        System.out.println("New array: " + Arrays.toString(arr));
+        arr = delete(7,arr);
+        System.out.println("Array: " + Arrays.toString(arr));
     }
 }
