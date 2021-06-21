bc26b1b0ca7aa782940c8a43ab123843466b642d
A0321I1-Lê Công Hậu <leconghau095@gmail.com>
2021-06-21 19:15:23 +0700

    nop bai tap

---
+++ module2/src/_3_mang_va_phuong_thuc_trong_java/bai_tap/TongCacSoOMotCotNhatDinh.java
@@ -0,0 +1,31 @@
+package _3_mang_va_phuong_thuc_trong_java.bai_tap;
+
+import java.util.Scanner;
+
+public class TongCacSoOMotCotNhatDinh {
+    public static int sumOfColumn(int[][] array, int column){
+        int sum = 0;
+        for(int i=0; i<array.length; i++){
+            for(int j=0; j<array[i].length; j++){
+                if(j==column) sum += array[i][j];
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size, column;
+        System.out.print("Enter a size: ");
+        size = scanner.nextInt();
+        int[][] arr = new int[size][size];
+        for(int i=0; i<size; i++){
+            for(int j=0; j<arr[i].length; j++){
+                System.out.print("Enter element arr[" + i + "," + j + "]: ");
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.print("Enter the column: ");
+        column = scanner.nextInt();
+        System.out.println("Sum of column " + column + " = " + sumOfColumn(arr,column));
+    }
+}
