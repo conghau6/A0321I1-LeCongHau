990a46e91b830d7a10346efdbccddb5f4f8cd7ce
A0321I1-Lê Công Hậu <leconghau095@gmail.com>
2021-06-21 18:49:31 +0700

    nop bai tap

--- module2/src/_3_mang_va_phuong_thuc_trong_java/thuc_hanh/TimGiaTriLonNhatTrongMang.java
+++ module2/src/_3_mang_va_phuong_thuc_trong_java/thuc_hanh/TimGiaTriLonNhatTrongMang.java
@@ -26,7 +26,7 @@ public class TimGiaTriLonNhatTrongMang {
 
         int max = array[0];
         int index = 0;
-        for(int i=0; i<array.length; i++){
+        for(int i=1; i<array.length; i++){
             if(array[i]>max){
                 max = array[i];
                 index = i+1;
--- module2/src/_3_mang_va_phuong_thuc_trong_java/thuc_hanh/TimPhanTuNhoNhatSuDungPhuongThuc.java
+++ module2/src/_3_mang_va_phuong_thuc_trong_java/thuc_hanh/TimPhanTuNhoNhatSuDungPhuongThuc.java
@@ -4,7 +4,7 @@ package _3_mang_va_phuong_thuc_trong_java.thuc_hanh;
 public class TimPhanTuNhoNhatSuDungPhuongThuc {
     public static int minValue(int[] array){
         int index = 0;
-        for(int i=0; i<array.length; i++){
+        for(int i=1; i<array.length; i++){
             if(array[i]<array[index]) index = i;
         }
         return index;
