ae28c26886ea7041295a9d546a7f843de87a57f7
A0321I1-Lê Công Hậu <leconghau095@gmail.com>
2021-08-09 19:25:38 +0700

    nop bai tap

---
+++ module2/src/_15_thuat_toan_sap_xep/thuc_hanh/minh_hoa_sap_xep_noi_bot/BubbleSort.java
@@ -0,0 +1,18 @@
+package _15_thuat_toan_sap_xep.thuc_hanh.minh_hoa_sap_xep_noi_bot;
+
+public class BubbleSort {
+    public void bubbleSort(int[] arr) {
+        boolean isSwap = true;
+        for (int i = 0; i < arr.length - 1 && isSwap; i++) {
+            isSwap = false;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j+1] = temp;
+                    isSwap = true;
+                }
+            }
+        }
+    }
+}
