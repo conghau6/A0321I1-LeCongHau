e8d2da133c6f50af1b6896e4577283a2dbcb0f3f
A0321I1-Lê Công Hậu <leconghau095@gmail.com>
2021-08-15 10:57:48 +0700

    nop bai tap

--- module2/src/_16_io_text_file/bai_tap/optional_doc_file_csv/DocFileCsv.java
+++ module2/src/_16_io_text_file/bai_tap/optional_doc_file_csv/DocFileCsv.java
@@ -2,6 +2,6 @@ package _16_io_text_file.bai_tap.optional_doc_file_csv;
 
 public class DocFileCsv {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("hau");
     }
 }
