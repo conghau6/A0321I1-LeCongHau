5dec4a89044ca4a66c8195863a4bc94424aeafb8
A0321I1-Lê Công Hậu <leconghau095@gmail.com>
2021-08-16 10:44:49 +0700

    nop bai tap

--- module2/src/case_study_furama_resort/_models/Customer.java
+++ module2/src/case_study_furama_resort/_models/Customer.java
@@ -1,4 +1,60 @@
 package case_study_furama_resort._models;
 
 public class Customer extends Person{
+    private String id;
+    private String name;
+    private int age;
+    private double balance;
+
+    public Customer() {
+    }
+
+    public Customer(String id, String name, int age, double balance) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.balance = balance;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", balance=" + balance +
+                '}';
+    }
 }
