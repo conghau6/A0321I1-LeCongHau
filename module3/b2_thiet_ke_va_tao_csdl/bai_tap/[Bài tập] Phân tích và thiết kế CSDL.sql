75ee5527daf48e6f16c4749bb54a0fd03e79503e
A0321I1-Lê Công Hậu <leconghau095@gmail.com>
2021-10-03 19:40:45 +0700

    nop bai tap

--- module3/b2_thiet_ke_va_tao_csdl/bai_tap/[Bài tập] Phân tích và thiết kế CSDL.sql
+++ module3/b2_thiet_ke_va_tao_csdl/bai_tap/[Bài tập] Phân tích và thiết kế CSDL.sql
@@ -1,89 +1,89 @@
 create database ql_oto;
 use ql_oto;
-create table productlines (
-	productLine varchar(50) not null,
-    textDescription text not null,
+create table product_lines (
+	product_line varchar(50) not null,
+    text_description text not null,
     image text not null,
-    primary key(productLine)
+    primary key(product_line)
 );
 create table offices (
-	officeCode varchar(10) not null,
+	office_code varchar(10) not null,
     city varchar(50) not null,
     phone varchar(50) not null,
-    addressLine1 varchar(50) not null,
-    addressLine2 varchar(50),
+    address_line1 varchar(50) not null,
+    address_line2 varchar(50),
     state varchar(50),
     country varchar(50) not null,
-    postalCode varchar(15) not null,
-    primary key(officeCode)
+    postal_code varchar(15) not null,
+    primary key(office_code)
 );
 create table employees (
-	employeeNumber int not null,
-    officeCode varchar(10),
-    lastName varchar(50) not null,
-    firstName varchar(50) not null,
+	employee_number int not null,
+    office_code varchar(10),
+    last_name varchar(50) not null,
+    first_name varchar(50) not null,
     email varchar(100) not null,
-    jobTitle varchar(50) not null,
-    reportTo int,
-    primary key(employeeNumber),
-    foreign key(officeCode) references offices(officeCode),
-    foreign key(reportTo) references employees(employeeNumber)
+    job_title varchar(50) not null,
+    report_to int,
+    primary key(employee_number),
+    foreign key(office_code) references offices(office_code),
+    foreign key(report_to) references employees(employee_number)
 );
 create table customers (
-	customerNumber int not null,
-    salesRepEmployeeNumber int,
+	customer_number int not null,
+    salesRepemployee_number int,
     customerName varchar(50) not null,
-    contactLastName varchar(50) not null,
-    contactFirstName varchar(50) not null,
+    contact_last_name varchar(50) not null,
+    contact_first_name varchar(50) not null,
     phone varchar(50) not null,
-    addressLine1 varchar(50) not null,
-    addressLine2 varchar(50),
+    address_line1 varchar(50) not null,
+    address_line2 varchar(50),
     city varchar(50) not null,
     state varchar(50) not null,
-    postalCode varchar(15) not null,
+    postal_code varchar(15) not null,
     country varchar(50) not null,
-    creditLimit float(2),
-    primary key(customerNumber),
-    foreign key(salesRepEmployeeNumber) references employees(employeeNumber)
+    credit_limit float(2),
+    primary key(customer_number),
+    foreign key(salesRepemployee_number) references employees(employee_number)
 );
 create table orders(
-	orderNumber int not null,
-    customerNumber int,
-    orderDate date not null,
-    requiredDate date not null,
-    shippedDate date,
+	order_number int not null,
+    customer_number int,
+    order_date date not null,
+    required_date date not null,
+    shipped_date date,
     status varchar(15) not null,
     comments text,
-    quantityOrdered int not null,
-    priceEach float(2) not null,
-	primary key (orderNumber),
-    foreign key(customerNumber) references customers (customerNumber)
+    quantity_ordered int not null,
+    price_each float(2) not null,
+	primary key (order_number),
+    foreign key(customer_number) references customers (customer_number)
 );
 create table payments (
-	customerNumber int,
-    checkNumber varchar(50) not null,
-    paymentDate date not null,
+	customer_number int,
+    check_number varchar(50) not null,
+    payment_date date not null,
     amount float(2) not null,
-    primary key(checkNumber),
-    foreign key (customerNumber) references customers(customerNumber)
+    primary key(check_number),
+    foreign key (customer_number) references customers(customer_number)
 );
 create table products (
-	productCode varchar(15) not null,
-    productLine varchar(50),
-    productName varchar(70) not null,
-    productScale varchar(10) not null,
-    productVendor varchar(50) not null,
-    productDescription text not null,
-    quantityInStock int not null,
-    buyPrice float(2) not null,
+	product_code varchar(15) not null,
+    product_line varchar(50),
+    product_name varchar(70) not null,
+    product_scale varchar(10) not null,
+    product_vendor varchar(50) not null,
+    product_description text not null,
+    quantityIn_stock int not null,
+    buy_price float(2) not null,
     MSRP float(2) not null,
-    primary key(productCode),
-    foreign key(productLine) references productlines(productLine)
+    primary key(product_code),
+    foreign key(product_line) references product_lines(product_line)
 );
 create table orderDetails (
-	orderNumber int,
-    productCode varchar(15),
-    primary key(ordernumber, productcode),
-    foreign key(ordernumber) references orders(ordernumber),
-    foreign key(productCode) references products(productCode)
+	order_number int,
+    product_code varchar(15),
+    primary key(order_number, product_code),
+    foreign key(order_number) references orders(order_number),
+    foreign key(product_code) references products(product_code)
 ); 
