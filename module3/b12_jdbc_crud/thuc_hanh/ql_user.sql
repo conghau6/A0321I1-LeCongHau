64cc040874133cd2b11c7083caaf472add63eecc
A0321I1-Lê Công Hậu <leconghau095@gmail.com>
2021-10-16 17:50:21 +0700

    nop bai tap

--- module3/b12_jdbc_crud/thuc_hanh/ql_user.sql
+++ module3/b12_jdbc_crud/thuc_hanh/ql_user.sql
@@ -9,3 +9,5 @@ create table users(
 );
 insert into users(name,email,country) values('Minh','minh@codegym.vn','Viet Nam');
 insert into users(name, email, country) values('Kante','kante@che.uk','Kenia');
+
+drop database th_demo;
