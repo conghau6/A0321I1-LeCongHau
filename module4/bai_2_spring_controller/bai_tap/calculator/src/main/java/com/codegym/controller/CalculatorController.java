b5e12cab39d0291f6de9e3210be9d2b7c7f071a0
A0321I1-Lê Công Hậu <leconghau095@gmail.com>
2021-11-14 22:48:51 +0700

    nop bai tap

--- module4/bai_2_spring_controller/bai_tap/calculator/src/main/java/com/codegym/controller/CalculatorController.java
+++ module4/bai_2_spring_controller/bai_tap/calculator/src/main/java/com/codegym/controller/CalculatorController.java
@@ -1,12 +1,42 @@
 package com.codegym.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CalculatorController {
-    @GetMapping
+    @GetMapping("/")
     public String showForm(){
         return "home";
+    }
+
+    @PostMapping("/calculate")
+    public ModelAndView calculator(@RequestParam("firstNumber") String firstNumber, @RequestParam("secondNumber") String secondNumber,
+                                   @RequestParam("operation") String operation){
+        ModelAndView modelAndView = new ModelAndView("home");
+        int number1 = Integer.parseInt(firstNumber);
+        int number2 = Integer.parseInt(secondNumber);
+        int result = 0;
+        switch (operation) {
+            case "Addition(+)":
+                result = number1 + number2;
+                break;
+            case "Subtraction(-)":
+                result = number1 - number2;
+                break;
+            case "Multiplication(+)":
+                result = number1 * number2;
+                break;
+            case "Division(/)":
+                result = number1 / number2;
+                break;
+        }
+        modelAndView.addObject("result",result);
+        return modelAndView;
     }
 }
--- module4/bai_2_spring_controller/bai_tap/calculator/src/main/webapp/WEB-INF/views/home.jsp
+++ module4/bai_2_spring_controller/bai_tap/calculator/src/main/webapp/WEB-INF/views/home.jsp
@@ -12,10 +12,18 @@
 </head>
 <body>
 <h1>Calculator</h1>
-<form action="">
-    <input type="text">
-    <input type="text">
-    <input type="submit">
+<form action="/calculate" method="post">
+    <div>
+        <input type="text" name="firstNumber">
+        <input type="text" name="secondNumber">
+    </div>
+    <div>
+        <input type="submit" name="operation" value="Addition(+)">
+        <input type="submit" name="operation" value="Subtraction(-)">
+        <input type="submit" name="operation" value="Multiplication(+)">
+        <input type="submit" name="operation" value="Division(/)">
+    </div>
 </form>
+<p>Result: ${result}</p>
 </body>
 </html>
