ccc0889181e95cf53d6c53883803b66ce51c9c1f
A0321I1-Lê Công Hậu <leconghau095@gmail.com>
2021-11-20 17:51:13 +0700

    nop bai tap

--- module4/bai_3_views_thymeleaf/bai_tap/quan_ly_san_pham/src/main/java/com/codegym/controller/ProductController.java
+++ module4/bai_3_views_thymeleaf/bai_tap/quan_ly_san_pham/src/main/java/com/codegym/controller/ProductController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/product")
+@RequestMapping({"/","/product"})
 public class ProductController {
     @Autowired
     ProductRepositories productRepositories;
