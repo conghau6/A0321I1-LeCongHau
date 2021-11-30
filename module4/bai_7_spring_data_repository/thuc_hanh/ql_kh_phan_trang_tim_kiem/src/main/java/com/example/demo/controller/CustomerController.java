68e9cac48286e253c750be80d351613e4b1978cf
A0321I1-Lê Công Hậu <leconghau095@gmail.com>
2021-11-30 08:55:32 +0700

    nop bai tap

---
+++ module4/bai_7_spring_data_repository/thuc_hanh/ql_kh_phan_trang_tim_kiem/src/main/java/com/example/demo/controller/CustomerController.java
@@ -0,0 +1,73 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Customer;
+import com.example.demo.model.Province;
+import com.example.demo.service.CustomerService;
+import com.example.demo.service.ProvinceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping({"/customer"})
+public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private ProvinceService provinceService;
+
+    @GetMapping("")
+    public ModelAndView listCustomers(@PageableDefault(size = 3) Pageable pageable){
+        return new ModelAndView("customer/index", "listCustomer",customerService.findAll(pageable));
+    }
+
+    @ModelAttribute("provinces")
+     public List<Province> provinces(){
+        return provinceService.findAll();
+    }
+
+    @GetMapping("/create")
+    public ModelAndView createCustomer(){
+        return new ModelAndView("customer/create","customer",new Customer());
+    }
+
+    @PostMapping("/create")
+    public ModelAndView createCustomer(@ModelAttribute("customer") Customer customer){
+        customerService.save(customer);
+        ModelAndView modelAndView = new ModelAndView("customer/create");
+        modelAndView.addObject("customer", new Customer());
+        modelAndView.addObject("message","Thêm thành công");
+        return modelAndView;
+    }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView editCustomer(@PathVariable("id") Long id){
+        return new ModelAndView("customer/edit","customer",customerService.findById(id));
+    }
+
+    @PostMapping("/edit")
+    public ModelAndView editCustomer(@ModelAttribute("customer") Customer customer){
+        customerService.save(customer);
+        ModelAndView modelAndView = new ModelAndView("customer/edit");
+        modelAndView.addObject("customer", customer);
+        modelAndView.addObject("message","Sủa thành công");
+        return modelAndView;
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView deleteCustomer(@PathVariable("id") Long id){
+        return new ModelAndView("customer/delete","customer",customerService.findById(id));
+    }
+
+    @PostMapping("/delete")
+    public String deleteCustomer(@ModelAttribute("customer") Customer customer){
+        customerService.remove(customer.getId());
+        return "redirect:/customer/";
+    }
+}
---
+++ module4/bai_7_spring_data_repository/thuc_hanh/ql_kh_phan_trang_tim_kiem/src/main/java/com/example/demo/service/CustomerService.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Customer;
+import com.example.demo.model.Province;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer> findAll();
+
+    Page<Customer> findAll(Pageable pageable);
+
+    Customer findById(Long id);
+
+    void save(Customer customer);
+
+    void remove(Long id);
+
+    List<Customer> findAllByProvince(Province province);
+}
