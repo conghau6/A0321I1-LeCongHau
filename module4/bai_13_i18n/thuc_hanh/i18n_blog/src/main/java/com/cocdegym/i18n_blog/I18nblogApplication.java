c3ee6ee3c89b56eff2ceb2124382fa3cd9e9672a
A0321I1-Lê Công Hậu <leconghau095@gmail.com>
2021-12-18 10:28:36 +0700

    nop bai tap

---
+++ module4/bai_13_i18n/thuc_hanh/i18n_blog/src/main/java/com/cocdegym/i18n_blog/I18nblogApplication.java
@@ -0,0 +1,13 @@
+package com.cocdegym.i18n_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I18nblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(I18nblogApplication.class, args);
+    }
+
+}
---
+++ module4/bai_13_i18n/thuc_hanh/i18n_blog/src/test/java/com/cocdegym/i18n_blog/I18nblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocdegym.i18n_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I18nblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
